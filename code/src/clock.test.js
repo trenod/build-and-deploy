d27f8599f97999550a163cb92a40649adc74bbac
trenod <torewb@gmail.com>
2024-11-12 18:53:17 +0100

    trying a new test

--- code/src/clock.test.js
+++ code/src/clock.test.js
@@ -3,12 +3,19 @@ import { getSecondsLeftOfYear, getTimeString } from "./clock";
 test('getTimeString formats time string correctly', () => {
     const now = new Date(0, 0, 0, 10, 20, 30);
     const timeString = getTimeString(now);
-    expect(timeString).toBe('10:20:3');
+    expect(timeString).toBe('10:20:30');
 });
 
 test('getSecondsLeftOfYear returns the correct amount of seconds', () => {
     const now = new Date("2022-12-31T23:00:00.000Z");
     const timeString = getSecondsLeftOfYear(now);
-    expect(timeString).toBe(360);
+    expect(timeString).toBe(3600);
+});
+
+test('getInterval returns the correct interval', () => {
+    const now = new Date("2022-12-31T23:00:00.000Z");
+    const then = new Date("2022-12-31T23:00:01.000Z");
+    const timeString = then - now;
+    expect(timeString).toBe(1000);
 });
 
